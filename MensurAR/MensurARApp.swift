82be952446894b583c7a4e2a3ecf789608ad13c4
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-22 16:09:35 -0300

    Initial Commit

---
+++ MensurAR/MensurARApp.swift
@@ -0,0 +1,17 @@
+//
+//  MensurARApp.swift
+//  MensurAR
+//
+//  Created by Cem Akkaya on 22/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MensurARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
